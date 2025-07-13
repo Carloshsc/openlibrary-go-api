602ee0ef6546cbd8cf9e2e02ad6733e4cc52a77d
Carlos <carloshsc1998@gmail.com>
2025-07-13 17:41:22 -0300

    code refactoring, messages, and limit - new messages to help user - new error messages - add limit to number of books returned - code refactoring

--- api/main.go
+++ api/main.go
@@ -7,36 +7,28 @@ import (
     "log"
     "net/http"
     "net/url"
-    "regexp"
     "strconv"
     "strings"
     "sort"
-
     "github.com/urfave/negroni"
 )
 
+// Book structure with necessary information
 type Book struct {
     Title string `json:"title"`
     AuthorName []string `json:"author_name"`
     ReleaseYear int `json:"first_publish_year"`
     Languages []string `json:"language"`
-    	
 }
 
+// An array that will store books
 type OpenLibraryResponse struct {
     Docs []Book `json:"docs"`
 }
 
-func searchHandler(w http.ResponseWriter, r *http.Request) {
+func searchHandler(w http.ResponseWriter, r *http.Request) {    
 
-    var searchName string
-    var year int
-    var operator string
-    var isRange bool = false
-
-    var startYear int
-    var endYear int
-
+    // Get the info passed by the url and set book name
     query := r.URL.Query().Get("q")
     if query == "" {
         log.Println("Missing 'q' parameter")
@@ -44,6 +36,10 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
         return
     }
 
+    searchName := strings.TrimSpace(strings.ToLower(query))
+
+    // Get the info passed by the url and set pagination and page size
+    // If pagination is set but page size is not, default value will be 10
     pageStr := r.URL.Query().Get("page")
     pageSizeStr := r.URL.Query().Get("pageSize")
     paginate := false
@@ -62,35 +58,56 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
             pageSize = ps
         }
     }
+    
+    // Get the info passed by the url and set order
+    sortParam := r.URL.Query().Get("order")
+    sortAlpha := sortParam == "alph"
+    sortDec := sortParam == "dec"
 
-    reRange := regexp.MustCompile(`(?i)(.*?)\s+year\s*:\s*(\d{4})-(\d{4})`)
-    reSingle := regexp.MustCompile(`(?i)(.*?)\s+year\s*([<>=])\s*(\d{4})`)
+    // Get the info passed by the url and set year filter
+    yearParam := r.URL.Query().Get("year")
 
-    sortDec := strings.Contains(strings.ToLower(query), "sort:dec")
-    sortAlpha := strings.Contains(strings.ToLower(query), "sort:alph")
+    isRange := false
 
-    matches := reRange.FindStringSubmatch(query)
-    if len(matches) == 4 {
-        searchName = strings.Trim(matches[1], "\" ")
-        startYear, _ = strconv.Atoi(matches[2])
-        endYear, _ = strconv.Atoi(matches[3])
-        isRange = true
-    } else {
-        matches = reSingle.FindStringSubmatch(query)
-        if len(matches) == 4 {
-            searchName = strings.Trim(matches[1], "\" ")
-            operator = matches[2]
-            year, _ = strconv.Atoi(matches[3])
-            log.Println("Single filter - term:", searchName, "op:", operator, "year:", year)
-        } else {
-            searchName = strings.Trim(query, "\" ")
+    var year int
+    var startYear int
+    var endYear int
+    var operator string
+
+    //If is range will use filter between 2 years
+    if strings.HasPrefix(yearParam, "Range") {
+        parts := strings.Fields(strings.TrimPrefix(yearParam, "Range"))
+        if len(parts) == 1 && strings.Contains(parts[0], "-") {
+            years := strings.Split(parts[0], "-")
+            if len(years) == 2 {
+                startYear, _ = strconv.Atoi(years[0])
+                endYear, _ = strconv.Atoi(years[1])
+                //Adjust year order if user passes largest value first
+                if startYear > endYear {
+                    startYear = endYear
+                    endYear =  startYear
+                }
+                isRange = true
+            }
+        }
+    } else if strings.HasPrefix(yearParam, "single") {
+        //If is a single year set operator
+        parts := strings.Fields(strings.TrimPrefix(yearParam, "single"))
+        if len(parts) == 2 {
+            operator = parts[0]
+            year, _ = strconv.Atoi(parts[1])
         }
     }
 
+    // Get the info passed by the url and set limit
+    limitStr := r.URL.Query().Get("limit")   
+
+    // Create the url to search the book
     q := url.QueryEscape(searchName)
     url := fmt.Sprintf("http://openlibrary.org/search.json?q=%s", q)
     log.Println("Searching:", url)
 
+    // Get the response
     resp, err := http.Get(url)
     if err != nil {
         log.Println("GET Error:", err)
@@ -99,6 +116,7 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
     }
     defer resp.Body.Close()
 
+    // Reads the response
     body, err := ioutil.ReadAll(resp.Body)
     if err != nil {
         log.Println("Error reading response:", err)
@@ -106,6 +124,7 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
         return
     }
 
+    // Store books data into an array
     var libResp OpenLibraryResponse
     if err := json.Unmarshal(body, &libResp); err != nil {
         log.Println("JSON parse Error:", err)
@@ -114,12 +133,12 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
     }
 
     results := []Book{}
+    // Get the right books
     for _, book := range libResp.Docs {
-        log.Println("book:", book)
-        //Sometimes the API returns titles that don't contain the correct name
-        //this ensures that all returned titles have the requested name
+        // Sometimes the API returns titles that don't contain the correct name
+        // This ensures that all returned titles have the requested name
         if strings.Contains(strings.ToLower(book.Title), strings.ToLower(searchName)) {
-
+            // Filter books by year filter, if set
             if isRange {
                 if book.ReleaseYear < startYear || book.ReleaseYear > endYear {
                     continue
@@ -144,6 +163,7 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
         }
     }
 
+    // Order the books
     if sortAlpha {
         sort.Slice(results, func(i, j int) bool {
             return strings.ToLower(results[i].Title) < strings.ToLower(results[j].Title)
@@ -157,14 +177,23 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
         })
     }
 
+    // If limit is set get the first number of books requested
     total := len(results)
-    if paginate {
+    if limitStr != "" {
+        if lim, err := strconv.Atoi(limitStr); err == nil && lim > 0 && lim < total {
+            results = results[:lim]
+            total = len(results)
+        }
+    }
+
+    // Create an array for pagination
+    if paginate && total > 0{
 
         start := (page - 1) * pageSize
         end := start + pageSize
 
         if start >= total {
-            // Invalid page, return response to access right message
+            // Invalid page, return response to show right message
             noBooks := Book{
                 Title:       "[No books on this page]",
                 AuthorName:  []string{"N/A"},
@@ -188,23 +217,31 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
         }
     }
 
+    // Create the response
     response := map[string]interface{}{
         "total": total,
         "books": results,
     }
 
+    // Return the information
     w.Header().Set("Content-Type", "application/json")
     json.NewEncoder(w).Encode(response)
 
 }
 
 func main() {
+
+    // Mux start
     mux := http.NewServeMux()
+
+    // Route declaration for /search
     mux.HandleFunc("/search", searchHandler)
 
+    // Negroni default declarations
     n := negroni.Classic()
     n.UseHandler(mux)
-
+    
+    // API message to show port that will be used
     log.Println("Server running on port 3000")
     http.ListenAndServe(":3000", n)
 }
--- runner/main.py
+++ runner/main.py
@@ -6,33 +6,102 @@ import re
 import requests
 import math
 
+#function to print help comand
+def print_help():
+    print('For Search books use: \'"book name"\'')
+    print('For Search books with filter by year [<,>] use: \'"book name" year>yyyy\'')
+    print('For Search books in a interval of years [:] use: \'"book name" year:yyyy-yyyy\'')
+    print('For Search books and order the list by: crescent (publish year, default), decrescent (publish year), alphabetical (book name) [dec, alph] use: \'"book name" dec\'')
+    print('For Search books and separate them into pages (default 10 books per page) [page] use: \'"book name" page=2\'')
+    print('For Search books, separate them into pages and change the number of books per page [pageSize] use: \'"book name" page=2 pageSize=20\'')
+    print('For Search books and limit the number of books returned [limit] use: \'"book name" limit=5\'')
+
+#function to print book list
+def print_book_list(bookList, start_index=1):
+    for i, info in enumerate(bookList, start=start_index):
+        title = info.get("title")
+
+        # There are cases where authors, years and languages are empty
+        authors_list = info.get("author_name")
+        if isinstance(authors_list, list) and len(authors_list) > 0:
+            authors = ", ".join(authors_list)
+        else:
+            authors = "Unknown Author"
+
+        year = info.get("first_publish_year")
+        if isinstance(year, int) and year > 0:
+            publiYear = str(year)
+        else:
+            publiYear = "Unknown Year"
+
+        languages = info.get("language")
+        if isinstance(languages, list) and len(languages) > 0:
+            lang = ", ".join(languages)
+        else:
+            lang = "Unknown languages"
+
+        print(f"{i}. {title}\n    - authors: {authors}\n    - publish year: {publiYear}\n    - available languages: {lang}\n")
+
+# Clear terminal screen when running the runner
+os.system("cls" if os.name == "nt" else "clear")
+
+#handling user input errors
 if len(sys.argv) < 2:
-    print("Usage: runner <search terms>")
+    print('Error: Invalid entry, please follow the examples below:')
+    print_help()
     sys.exit(1)
 
+#print a list of comands if -h or --help is sent
+if sys.argv[1] in ("--help", "-h"):
+    print_help()
+    sys.exit(0)
+
+#get inputs
 args = sys.argv[1:]
-
-page = 1
-paginated = False
-
 userInput = " ".join(args)
-pageSize = 10
 
-print(f"Received input: {userInput}")
+#get book name
 titleMatch = re.search(r'"([^"]+)"', userInput)
 
 if titleMatch:
     bookName = titleMatch.group(1)
-    print(f"titleMatch: {bookName}")
 else:
-    print("Error: Book title must be inside double quotes (e.g., \"Lord of the Rings\")")
+    print('Error: Invalid entry, please follow the examples below:')
+    print_help()
     sys.exit(1)
 
-decrescent = "dec" in userInput.lower()
-alphaOrder = "alph" in userInput.lower()
+#verify and set year filter if user want to use
+operator=""
+yearFilter=""
 
-rangeMatch = re.search(r'year\s*:\s*(\d{4})-(\d{4})', userInput, re.IGNORECASE)
-singleMatch = re.search(r'year\s*([<>=])\s*(\d{4})', userInput, re.IGNORECASE)
+rangeMatch = re.search(r'year\s*:\s*(\d+)-(\d+)', userInput, re.IGNORECASE)
+singleMatch = re.search(r'year\s*([<>=])\s*(-?\d+)', userInput, re.IGNORECASE)
+
+if rangeMatch:
+    startYear = int(rangeMatch.group(1))
+    endYear = int(rangeMatch.group(2))
+    if startYear < 1000 or endYear < 1000:
+        print("Error: Years in range must be 4-digit positive numbers.")
+        sys.exit(1)
+
+    if startYear > endYear:
+        startYear = rangeMatch.group(2)
+        endYear = rangeMatch.group(1)
+        
+    yearFilter = f'Range {startYear}-{endYear}'
+
+elif singleMatch:
+    operator = singleMatch.group(1)
+    year = int(singleMatch.group(2))
+    if year < 1000:
+        print("Error: Year must be a 4-digit positive number.")
+        sys.exit(1)
+    yearFilter = f"single {operator} {year}"
+
+#verify and set pagination if user want to use
+page = 1
+paginated = False
+pageSize = 10
 
 pageMatch = re.search(r'page\s*=\s*(\d+)', userInput, re.IGNORECASE)
 if pageMatch:
@@ -43,53 +112,46 @@ pageSizeMatch = re.search(r'pagesize\s*=\s*(\d+)', userInput, re.IGNORECASE)
 if pageSizeMatch:
     pageSize = int(pageSizeMatch.group(1))
 
-operator=""
-textFilter=""
+#verify and set limit if user want to use
+limitMatch = re.search(r'limit\s*=\s*(\d+)', userInput, re.IGNORECASE)
+limit = None
+if limitMatch:
+    limit = int(limitMatch.group(1))
 
-if rangeMatch:
-    startYear = rangeMatch.group(1)
-    endYear = rangeMatch.group(2)
-    if startYear > endYear:
-        startYear = rangeMatch.group(2)
-        endYear = rangeMatch.group(1)
-    query = f'{bookName} year:{startYear}-{endYear}'
-elif singleMatch:
-    operator = singleMatch.group(1)
-    year = singleMatch.group(2)
-    query = f"{bookName} year {operator} {year}"
-else:
-    query = bookName
+#verify and set order if user want to use
+order=""
+decrescent = "dec" in userInput.lower()
+alphaOrder = "alph" in userInput.lower()
 
 if decrescent:
-    query += " sort:dec"
+    order = "dec"
 
 if alphaOrder:
-    query += " sort:alph"
+    order = "alph"
 
-port = os.environ.get("API_PORT", "3000")
-host = os.environ.get("API_HOST", "api")
-
+#set request url to go api
 if paginated:
-    url = f"http://{host}:{port}/search?q={query}&page={page}&pageSize={pageSize}"
+    url = f"http://api:3000/search?q={bookName}&year={yearFilter}&order={order}&page={page}&pageSize={pageSize}&limit={limit}"
 else:
-    url = f"http://{host}:{port}/search?q={query}"
+    url = f"http://api:3000/search?q={bookName}&year={yearFilter}&order={order}&limit={limit}"
 
-print(f"url' {url}':")
-
+#set message to correspondent filter
+textFilter=""
 if rangeMatch:
     textFilter = f" - Published between {startYear} and {endYear}"
 elif singleMatch:
     match operator:
         case "<":
-            textFilter=" - Published before " + year
+            textFilter=" - Published before " + str(year)
         case ">":
-            textFilter=" - Published after " + year
+            textFilter=" - Published after " + str(year)
         case "=":
-            textFilter=" - Published in " + year
+            textFilter=" - Published in " + str(year)
         case _:
             textFilter=""
 
 try:
+    #get go api response and set total and books
     response = requests.get(url)
     response.raise_for_status()
     result = response.json()
@@ -97,13 +159,16 @@ try:
     total = result.get("total", 0)
     bookList = result["books"]
        
+    #error message if no book found
     if not bookList:
-        print(f"No books found with that name")
+        print(f"No books found with the name {bookName}")
     else:
         
+        #if user want pagination
         if paginated:
             totalPages = math.ceil(total / pageSize)
 
+            #message if the user wants to access a page that doesn't exist
             if page > totalPages:
                 print(f"Page {page} does not exist. There are only {totalPages} pages available.\n")
                 sys.exit(1)
@@ -120,57 +185,14 @@ try:
             
             startIndex = (page - 1) * pageSize + 1
             endIndex = min(startIndex + len(bookList) - 1, total)
+
             print(f"Showing books {startIndex}–{endIndex} of {total}.\n")
-
-            for i, info in enumerate(bookList, start=startIndex):
-                title = info.get("title")
-
-                # There are cases where authors, years and languages are empty
-                authors_list = info.get("author_name")
-                if isinstance(authors_list, list) and len(authors_list) > 0:
-                    authors = ", ".join(authors_list)
-                else:
-                    authors = "Unknown Author"
-
-                year = info.get("first_publish_year")
-                if isinstance(year, int) and year > 0:
-                    publiYear = str(year)
-                else:
-                    publiYear = "Unknown Year"
-
-                languages = info.get("language")
-                if isinstance(languages, list) and len(languages) > 0:
-                    lang = ", ".join(languages)
-                else:
-                    lang = "Unknown languages"
-
-                print(f"{i}. {title}\n    - authors: {authors}\n    - publish year: {publiYear}\n    - available languages: {lang}\n")       
+            print_book_list(bookList, startIndex)     
 
         else:
             print(f"\n{len(bookList)} Books found for: {bookName}{textFilter}\n")
+            print_book_list(bookList)
 
-            for i, info in enumerate(bookList, 1):
-                title = info.get("title")
-
-                # There are cases where authors, years and languages are empty
-                authors_list = info.get("author_name")
-                if isinstance(authors_list, list) and len(authors_list) > 0:
-                    authors = ", ".join(authors_list)
-                else:
-                    authors = "Unknown Author"
-
-                year = info.get("first_publish_year")
-                if isinstance(year, int) and year > 0:
-                    publiYear = str(year)
-                else:
-                    publiYear = "Unknown Year"
-
-                languages = info.get("language")
-                if isinstance(languages, list) and len(languages) > 0:
-                    lang = ", ".join(languages)
-                else:
-                    lang = "Unknown languages"
-
-                print(f"{i}. {title}\n    - authors: {authors}\n    - publish year: {publiYear}\n    - available languages: {lang}\n")
+#error handling
 except requests.RequestException as e:
     print("Something went wrong, the requested book could not be found.")
